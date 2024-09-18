e38709689f76c98fcc8927962eb68a0a369f58c0
Haridev <haridev.8281@outlook.com>
2024-09-18 16:56:07 +0530

    Updates Facts component to taiwind

--- client/src/components/Facts.jsx
+++ client/src/components/Facts.jsx
@@ -1,77 +1,41 @@
 function Facts() {
   return (
     <>
-      {/* <!-- Facts Start --> */}
-      <div className="container-xxl py-5">
-        <div className="container">
-          <div
-            className="wow fadeInUp mx-auto text-center"
-            data-wow-delay="0.1s"
-            style={{ maxWidth: 500 }}
-          >
-            <p className="text-primary text-uppercase mb-2">Why Choose Me!</p>
-            <h1 className="display-6 mb-5">
-              The Leading Design Studio In The State
-            </h1>
+      <section className="flex min-h-[80vh] flex-col items-center">
+        <h1 className="my-8 text-primaryBlue">WHY CHOOSE ME</h1>
+        <h4 className="mb-14 px-2 text-center text-4xl font-thin">
+          The Leading Design Studio In The State
+        </h4>
+        <div className="flex flex-col sm:flex-row sm:items-end sm:justify-around">
+          {/* First Card */}
+          <div className="m-2 flex h-[80vw] flex-col items-center justify-center bg-imageFrame sm:h-[30vw] sm:w-1/3">
+            <p className="text-7xl text-primaryBlue">15</p>
+            <p className="my-6 font-bold">Award Winning</p>
+            <p className="px-10 text-center">
+              Have participated in various competitions as well as been
+              recognized in various exhibitions for my works
+            </p>
           </div>
-          <div className="row g-3">
-            <div
-              className="col-lg-4 col-md-6 pt-lg-5 wow fadeInUp"
-              data-wow-delay="0.1s"
-            >
-              <div className="fact-item bg-light h-100 p-5 text-center">
-                <h1
-                  className="display-2 text-primary mb-3"
-                  data-toggle="counter-up"
-                >
-                  15
-                </h1>
-                <h4 className="mb-3">Award Winning</h4>
-                <span>
-                  Have participated in various competitions as well as been
-                  recognized in various exhibitions for my works
-                </span>
-              </div>
-            </div>
-            <div
-              className="col-lg-4 col-md-6 wow fadeInUp"
-              data-wow-delay="0.3s"
-            >
-              <div className="fact-item bg-light h-100 p-5 text-center">
-                <h1
-                  className="display-2 text-primary mb-3"
-                  data-toggle="counter-up"
-                >
-                  10
-                </h1>
-                <h4 className="mb-3">Years Experience</h4>
-                <span>
-                  Have worked for over a decade and satisfied customers
-                  throughout the state as well as the country
-                </span>
-              </div>
-            </div>
-            <div
-              className="col-lg-4 col-md-6 pt-lg-5 wow fadeInUp"
-              data-wow-delay="0.5s"
-            >
-              <div className="fact-item bg-light h-100 p-5 text-center">
-                <h1
-                  className="display-2 text-primary mb-3"
-                  data-toggle="counter-up"
-                >
-                  500
-                </h1>
-                <h4 className="mb-3">Happy Clients</h4>
-                <span>
-                  More than 500 happy customers throughout my 10 years career of
-                  creating art
-                </span>
-              </div>
-            </div>
+
+          <div className="m-2 flex h-[80vw] flex-col items-center justify-center bg-imageFrame sm:h-[35vw] sm:w-1/3">
+            <p className="text-7xl text-primaryBlue">10</p>
+            <p className="my-6 font-bold">Years Experience</p>
+            <p className="px-10 text-center">
+              Have worked for over a decade and satisfied customers throughout
+              the state as well as the country
+            </p>
+          </div>
+
+          <div className="m-2 flex h-[80vw] flex-col items-center justify-center bg-imageFrame sm:h-[30vw] sm:w-1/3">
+            <p className="text-7xl text-primaryBlue">500</p>
+            <p className="my-6 font-bold">Happy Clients</p>
+            <p className="px-10 text-center">
+              More than 500 happy customers throughout my 10 years career of
+              creating art
+            </p>
           </div>
         </div>
-      </div>
+      </section>
       {/* <!-- Facts End --> */}
     </>
   );
